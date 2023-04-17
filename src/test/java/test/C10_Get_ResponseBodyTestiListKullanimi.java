6c35438182184253e09ad0a0213d191ebfb0fbf2
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-04-17 11:54:42 +0200

    API

--- src/test/java/test/C10_Get_ResponseBodyTestiListKullanimi.java
+++ src/test/java/test/C10_Get_ResponseBodyTestiListKullanimi.java
@@ -1,5 +1,12 @@
 package test;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class C10_Get_ResponseBodyTestiListKullanimi {
     /*
             http://dummy.restapiexample.com/api/v1/employees url'ine bir GET request yolladigimizda
@@ -12,4 +19,20 @@ public class C10_Get_ResponseBodyTestiListKullanimi {
                 ve girilen yaslar icinde 61,40 ve 30 degerlerinin oldugunu test edin
             test edin.
      */
+
+    @Test
+    public void get01(){
+
+        String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+        Response response= given().when().get(url);
+        response.prettyPrint();
+
+        response.then().assertThat().statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.id", Matchers.hasSize(24),
+                        "data.employee_name", Matchers.hasItem("Ashton cox"),
+                        "data.employee_age", Matchers.hasItems(61,40,30));
+
+    }
 }
