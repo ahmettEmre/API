ac456e588f4388197ec0f252d786e583dfd2d853
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-04-19 12:35:00 +0200

    API

---
+++ src/test/java/test/C21_Post_TestDataKullanimi.java
@@ -0,0 +1,52 @@
+package test;
+
+import baseURL.HerokuAppBaseUrl;
+import io.restassured.path.json.JsonPath;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C21_Post_TestDataKullanimi extends HerokuAppBaseUrl {
+
+    /*
+    https://restful-booker.herokuapp.com/booking url’ine asagidaki body'ye sahip
+    bir POST request gonderdigimizde donen response’un status kodunu ve id haric
+    body'sinin asagidaki gibi oldugunu test edin.
+    Request body
+          {
+          "firstname" : "Ali",
+          "lastname" : “Bak",
+          "totalprice" : 500,
+          "depositpaid" : false,
+          "bookingdates" : {
+                      "checkin" : "2021-06-01",
+                      "checkout" : "2021-06-10"
+                        },
+          "additionalneeds" : "wi-fi"
+           }
+    Expected Body
+    {
+    "bookingid":24,
+    "booking":{
+            "firstname":"Ali",
+            "lastname":"Bak",
+            "totalprice":500,
+            "depositpaid":false,
+            "bookingdates":{
+                            "checkin":"2021-06-01",
+                            "checkout":"2021-06-10"
+                            },
+            "additionalneeds":"wi-fi"
+               }
+    }
+     */
+
+    @Test
+    public void post01(){
+
+        specHerokuApp.pathParam("pp1","booking");
+
+
+
+
+    }
+}
---
+++ src/test/java/testData/TestDataHerokuApp.java
@@ -0,0 +1,24 @@
+package testData;
+
+import org.json.JSONObject;
+
+public class TestDataHerokuApp {
+
+    public JSONObject bookingDatesJson(){
+        JSONObject bookingdates = new JSONObject();
+        bookingdates.put("checkin","2021-06-01");
+        bookingdates.put("checkout","2021-06-10");
+        return bookingdates;
+    }
+
+    public JSONObject bookingJson(){
+        JSONObject reqBody=new JSONObject();
+        reqBody.put("firstname","Ali");
+        reqBody.put("lastname","Bak");
+        reqBody.put("totalprice",500);
+        reqBody.put("depositpaid",false);
+        reqBody.put("bookingdates",bookingDatesJson());
+        reqBody.put("additionalneeds","wi-fi");
+        return reqBody;
+    }
+}
